c16b2ec860885f641b27505d8f579b36708d1831
pranavpv773 <pranavpv773@gmail.com>
2022-11-24 19:30:48 +0530

    global screen created and bottom navigation implemented successfully

---
+++ lib/app/modules/global/view_model/global_notifier.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:marlo_app/app/modules/chart/chart.dart';
+import 'package:marlo_app/app/modules/contracts/screen_one/view/screen_one.dart';
+import 'package:marlo_app/app/modules/home/home.dart';
+import 'package:marlo_app/app/modules/loans/loan.dart';
+import 'package:marlo_app/app/modules/teams/teams.dart';
+
+class GlobalNotifier with ChangeNotifier {
+  int selectIndex = 2;
+  List pages = [
+    const HomeScreen(),
+    const LoanScreen(),
+    const ScreenOne(),
+    const TeamsScreen(),
+    const ChartScreen(),
+  ];
+  onIndexChange(int value) {
+    selectIndex = value;
+    notifyListeners();
+  }
+}
--- lib/app/modules/splash/view_model/splash_notifier.dart
+++ lib/app/modules/splash/view_model/splash_notifier.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/widgets.dart';
 import 'package:marlo_app/app/app_routes/app_routes.dart';
-import 'package:marlo_app/app/modules/screen_one/view/screen_one.dart';
+import 'package:marlo_app/app/modules/global/view/global.dart';
 
 class SplashNotifier with ChangeNotifier {
+  SplashNotifier() {
+    goScreenOne();
+  }
   goScreenOne() async {
     await Future.delayed(
       const Duration(
         seconds: 3,
       ),
     );
-    AppRoutes.removeScreen(
-      screen: const ScreenOne(),
+    AppRoutes.removeScreenUntil(
+      screen: const GlobalScreen(),
     );
   }
 }
